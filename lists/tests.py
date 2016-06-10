99fde37fd6564a850f7153427d6a2e2ea5e56528
rh01 <1048157315@qq.com>
2016-06-11 00:18:08 +0800

    first unit test and url mapping.dump view

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
 class SmokeTest(TestCase):
     def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
