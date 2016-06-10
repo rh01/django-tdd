fe13b543762b32aa16973453e8dd976cc532d555
rh01 <1048157315@qq.com>
2016-06-11 00:48:58 +0800

    Fi=unctional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 # -*- coding:utf8 -*-
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+
 
 
 class NewVisionTest(unittest.TestCase):
@@ -19,10 +21,27 @@ class NewVisionTest(unittest.TestCase):
         
         # 他注意到网页的标题和头部都包含"TO-DO"这个词
         self.assertIn("To-Do" ,self.browser.title)
-        self.fail("Finish the test")
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
+        # 应用邀请他输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
+
         # 他在一个文本框中输入了"Buy fish"
+        inputbox.send_keys(Keys.ENTER)
+
+
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: By fish' for row in rows)
+        )
+
         
         # 他按下Enter键之后，页面更新了
         # 待办事项表格中显示了"1: Buy fish"
@@ -38,6 +57,7 @@ class NewVisionTest(unittest.TestCase):
         # 他看到网站为他生成了一个唯一的URL
         # 而且页面中有一些文字解说这个功能
 
+        self.fail("Finish the test")
 
 
         # 他访问那个URL，发现他的待办事项列表还在
